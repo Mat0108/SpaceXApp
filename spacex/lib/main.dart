2ab041f14b7bb92718fbd0a3b24a35574b8d5940
Coumbadiankha <coumba.diankha@hotmail.fr>
2024-01-31 09:18:45 +0100

    Update main.dart

--- spacex/lib/main.dart
+++ spacex/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/capsule.dart';
 import './pages/launches.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       ),
       home: HomePage(),
       routes: {
+        '/capsule': (context) => const CapsulesPage(),
         '/launches': (context) => const LaunchesPage(),
       },
     );
